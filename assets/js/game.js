1d06169a8e1dc11b8d6195e6208483efab1b779a
Aaron Neslin <aneslin125@gmail.com>
2021-12-01 20:38:20 -0500

    added start and end game features

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,25 +69,59 @@ var fight = function(enemyName) {
   }
 };
 
-for(var i = 0 ; i < enemyNames.length; i++){
-  if (playerHealth > 0) {
+var startGame = function() {
+  debugger;
+  //reset player
+  playerHealth = 100;
+  playerAttack=10;
+  playerMoney=10;
+
+  for(var i = 0 ; i < enemyNames.length; i++){
+    if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
     // pick new enemy to fight based on the index of the enemyNames array
-    var pickedEnemyName = enemyNames[i];
+      var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+      enemyHealth = 50;
 
     // use debugger to pause script from running and check what's going on at that moment in the code
     // debugger;
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-    fight(pickedEnemyName);
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      endGame();
+   }
   }
-  else {
-    window.alert("You have lost your robot in battle! Game Over!");
-    break;
-  }
+  //play again
+  endGame();
+};
+
+var endGame = function(){
+
+if (playerHealth > 0){
+window.alert("Great job, you survived the game. You now have a score of " + playerMoney + ".");
 }
+else{
+  window.alert("You lost your robot battle.");
+}
+var playAgainConfirm = window.confirm("Would you like to play again?");
+if (playAgainConfirm) {
+  //restart game
+  startGame();
+}
+else {
+  window.alert("thank you for player robot gladiators come back soon"); 
+}
+}
+
+
+
+
+startGame();
+ 
